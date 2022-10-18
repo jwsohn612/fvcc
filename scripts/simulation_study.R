28e80798d14cbf7d711dd483614af9cf2349a999
Jinwon Sohn <jwsohn612@gmail.com>
2022-10-18 17:56:45 -0400

    simulation study

--- scripts/simulation_study.R
+++ scripts/simulation_study.R
@@ -21,21 +21,30 @@ output <- cvarpyp(
 )
 
 # If burn = NULL, it discard a half of samples as default.
+par(mfrow = c(1,2))
 plot(output$fixed_effect, position = 1, burn = NULL)
-plot(output$random_effect,
-     row_position = 1,
-     col_position = 1)
-plot(output$cluster)
-plot(
-  output$varying_coefficient,
-  cluster_number = 5,
-  variable_number = 1
-)
-plot(
-  output$latent_location,
-  cluster_number = 5,
-  variable_number = 1,
-  time_range = output$time_range,
-  knot_position = output$knot_position
-)
+plot(output$fixed_effect, position = 2, burn = NULL)
+
+par(mfrow = c(1,3))
+plot(output$random_effect, row_position = 1, col_position = 1)
+plot(output$random_effect, row_position = 1, col_position = 2)
+plot(output$random_effect, row_position = 2, col_position = 2)
+
+# check the estimated cluster labels for all subjects and then put cluster_number to draw varying coefficients
+table(plot(output$cluster))
+par(mfcol=c(2,3))
+plot(output$varying_coefficient, cluster_number = 1, variable_number = 1)
+plot(output$varying_coefficient, cluster_number = 1, variable_number = 2)
+plot(output$varying_coefficient, cluster_number = 6, variable_number = 1)
+plot(output$varying_coefficient, cluster_number = 6, variable_number = 2)
+plot(output$varying_coefficient, cluster_number = 3, variable_number = 1)
+plot(output$varying_coefficient, cluster_number = 3, variable_number = 2)
+
+par(mfcol=c(2,3))
+plot(output$latent_location, cluster_number = 1, variable_number = 1,time_range = output$time_range,knot_position = output$knot_position)
+plot(output$latent_location, cluster_number = 1, variable_number = 2,time_range = output$time_range,knot_position = output$knot_position)
+plot(output$latent_location, cluster_number = 6, variable_number = 1,time_range = output$time_range,knot_position = output$knot_position)
+plot(output$latent_location, cluster_number = 6, variable_number = 2,time_range = output$time_range,knot_position = output$knot_position)
+plot(output$latent_location, cluster_number = 3, variable_number = 1,time_range = output$time_range,knot_position = output$knot_position)
+plot(output$latent_location, cluster_number = 3, variable_number = 2,time_range = output$time_range,knot_position = output$knot_position)
 
